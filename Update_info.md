5c402fe20d73133f8bd4d2f6f9cb079f5e03c42b
polishfrog <dawidrozewski21@gmail.com>
2023-04-24 14:32:38 +0200

    Create first view for user. Configured project and created database.

--- Update_info.md
+++ Update_info.md
@@ -2,3 +2,6 @@
 [24-04-2023]
 	[10:00-14:00]
 	Start project, create simple files, testing system.
+	[14:00-14:30]
+	Create first view. Configure project and create database.
+	
--- crm/views.py
+++ crm/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views import View
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+
+
+class HomePage(View):
+    """
+    First web when user into to website
+    """
+    def get(self, request):
+        return render(request, 'base.html', locals())
+
